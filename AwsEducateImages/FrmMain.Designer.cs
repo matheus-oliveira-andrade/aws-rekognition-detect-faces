34666e027a409cdc531622fbb72954667d86f1c0
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-04-27 22:19:39 -0300

    Initial

---
+++ AwsEducateImages/FrmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace AwsEducateImages
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rtbRetorno = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1025, 603);
+            this.splitContainer1.SplitterDistance = 341;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rtbRetorno);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(341, 538);
+            this.panel2.TabIndex = 1;
+            // 
+            // rtbRetorno
+            // 
+            this.rtbRetorno.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbRetorno.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rtbRetorno.Location = new System.Drawing.Point(0, 0);
+            this.rtbRetorno.Name = "rtbRetorno";
+            this.rtbRetorno.Size = new System.Drawing.Size(341, 538);
+            this.rtbRetorno.TabIndex = 0;
+            this.rtbRetorno.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnImage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnImage.Location = new System.Drawing.Point(0, 0);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(341, 65);
+            this.btnImage.TabIndex = 0;
+            this.btnImage.Text = "Ler imagem";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.BtnImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(680, 603);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 603);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FrmMain";
+            this.Text = "Rekognition - Detect faces";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rtbRetorno;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ AwsEducateImages/FrmMain.cs
@@ -0,0 +1,125 @@
+﻿using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AwsEducateImages
+{
+    public partial class FrmMain : Form
+    {
+        // Essential
+        private AWSCredentials _awsCredentials;
+        private readonly RegionEndpoint _region = RegionEndpoint.USEast1;
+
+        // Rekognition bucket
+        private const string BUCKET_NAME = "my-bucket-5564785";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+
+            // Get credentials in machine
+            if (!chain.TryGetAWSCredentials("AWS Educate profile", out _awsCredentials))
+            {
+                MessageBox.Show("Error fetching credentials");
+            }
+        }
+
+        private async Task<bool> UploadImageToS3(string file)
+        {
+            GetCredentials();
+
+            try
+            {
+                AmazonS3Client s3Client = new AmazonS3Client(_awsCredentials, _region);
+                TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+
+                await fileTransferUtility.UploadAsync(file, BUCKET_NAME);
+
+                return true;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                MessageBox.Show($"Error encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+        }
+
+        private async Task DetectFaces(string image)
+        {
+            AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(_awsCredentials, _region);
+
+            DetectFacesRequest detectFacesRequest = new DetectFacesRequest()
+            {
+                Image = new Amazon.Rekognition.Model.Image()
+                {
+                    S3Object = new S3Object()
+                    {
+                        Name = Path.GetFileName(image),
+                        Bucket = BUCKET_NAME,
+                    }
+                },
+                Attributes = new List<string> { "ALL" },
+            };
+
+            try
+            {
+                DetectFacesResponse detectFacesResponse = await rekognitionClient.DetectFacesAsync(detectFacesRequest);
+                bool hasAll = detectFacesRequest.Attributes.Contains("ALL");
+
+                rtbRetorno.Clear();
+
+                foreach (FaceDetail face in detectFacesResponse.FaceDetails)
+                {
+                    rtbRetorno.AppendText(JsonConvert.SerializeObject(face, Formatting.Indented));
+
+                    Pen pen = new Pen(Color.Red);
+
+                    Graphics graphics = pictureBox1.CreateGraphics();
+                    graphics.DrawRectangle(pen,
+                                           face.BoundingBox.Left * pictureBox1.Image.Width,
+                                           face.BoundingBox.Top * pictureBox1.Image.Height,
+                                           face.BoundingBox.Width * pictureBox1.Image.Width,
+                                           face.BoundingBox.Height * pictureBox1.Image.Height);
+                }
+            }
+            catch (Exception ex)
+            {
+                rtbRetorno.AppendText(ex.Message);
+            }
+        }
+
+        private async void BtnImage_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+
+                await UploadImageToS3(openFileDialog1.FileName);
+                await DetectFaces(openFileDialog1.FileName);
+            }
+        }
+    }
+}
